0fb8b1a8d7d44dc4371895117fd32ad37eb2aa95
matheuschagas <matheus.chagas@acad.pucrs.br>
2017-09-22 23:52:28 -0300

    crianca

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var criancas = require('./routes/criancas');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/criancas', criancas);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
